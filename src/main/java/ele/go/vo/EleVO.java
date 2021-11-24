1c8ab9dd459ba3818c810c81c5f6ebe019c44358
dlgks <dlgks@LAPTOP-OVI016IU>
2021-11-24 19:55:20 +0900

    evMap 및 차트 올림

---
+++ src/main/java/ele/go/vo/EleVO.java
@@ -0,0 +1,182 @@
+package ele.go.vo;
+
+public class EleVO {
+	//전기차 충전소 공공데이터 오픈 api VO
+	private String busiNm,
+	note,	 
+	parkingFree,
+	stat, 	
+	busiCall, 
+	limitYn,	
+	useTime,	
+	lat,	
+	lng,	
+	addr,	
+	chgerType,	
+	statNm,
+	statId,	
+	chgerId,
+	location;
+	
+	public EleVO() {}
+	
+	
+	
+	
+	
+	public EleVO(String limitYn, String note, String parkingFree, String stat, String busiCall, String busiNm,
+			String useTime, String lat, String lng, String addr, String chgerType, String statNm, String statId,
+			String chgerId, String location) {
+		super();
+		this.limitYn = limitYn;
+		this.note = note;
+		this.parkingFree = parkingFree;
+		this.stat = stat;
+		this.busiCall = busiCall;
+		this.busiNm = busiNm;
+		this.useTime = useTime;
+		this.lat = lat;
+		this.lng = lng;
+		this.addr = addr;
+		this.chgerType = chgerType;
+		this.statNm = statNm;
+		this.statId = statId;
+		this.chgerId = chgerId;
+		this.location = location;
+	}
+
+
+
+
+
+	public String getLocation() {
+		return location;
+	}
+
+
+
+
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+
+
+
+
+	public String getLimitYn() {
+		return limitYn;
+	}
+
+	public void setLimitYn(String limitYn) {
+		this.limitYn = limitYn;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public String getParkingFree() {
+		return parkingFree;
+	}
+
+	public void setParkingFree(String parkingFree) {
+		this.parkingFree = parkingFree;
+	}
+
+	public String getStat() {
+		return stat;
+	}
+
+	public void setStat(String stat) {
+		this.stat = stat;
+	}
+
+	public String getBusiCall() {
+		return busiCall;
+	}
+
+	public void setBusiCall(String busiCall) {
+		this.busiCall = busiCall;
+	}
+
+	public String getBusiNm() {
+		return busiNm;
+	}
+
+	public void setBusiNm(String busiNm) {
+		this.busiNm = busiNm;
+	}
+
+	public String getUseTime() {
+		return useTime;
+	}
+
+	public void setUseTime(String useTime) {
+		this.useTime = useTime;
+	}
+
+	public String getLat() {
+		return lat;
+	}
+
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+
+	public String getLng() {
+		return lng;
+	}
+
+	public void setLng(String lng) {
+		this.lng = lng;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getChgerType() {
+		return chgerType;
+	}
+
+	public void setChgerType(String chgerType) {
+		this.chgerType = chgerType;
+	}
+
+	public String getStatNm() {
+		return statNm;
+	}
+
+	public void setStatNm(String statNm) {
+		this.statNm = statNm;
+	}
+
+	public String getStatId() {
+		return statId;
+	}
+
+	public void setStatId(String statId) {
+		this.statId = statId;
+	}
+
+	public String getChgerId() {
+		return chgerId;
+	}
+
+	public void setChgerId(String chgerId) {
+		this.chgerId = chgerId;
+	}
+	
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/chart.jsp
@@ -0,0 +1,118 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Chart</title>
+	<style>
+	
+		*{margin: 0; padding: 0;}	
+			
+			
+		#chart_div{
+			margin : 0px;
+			padding : 0px;
+			background-color: #f9f9f9;
+			margin-top: 50px;
+		}
+		
+		#chart_area{
+			width: 70%;
+			height: 600px;
+			margin: auto;
+			font-size: 20px;
+			margin-top: 25px;
+			
+		}
+		#chartname{
+			font-size: 35px;
+			line-height: 3.5;
+			text-align: center;
+			width: 100%;
+			height: 50px;
+			font-weight: bold;
+			color: gray;
+			background-color: #f9f9f9;
+			font-family: monospace;
+			margin-top: 50px;
+		}
+		#middleback{
+			height: 450;
+			background-color: #f9f9f9;
+		}
+		
+	</style>
+	<header>
+	
+	</header>
+</head>
+<body>
+		
+	<!-- !!!!!!!실행전 파이썬 플라스크 구동해야 함!!!!!-->	
+		
+		
+	<div id="chart_div">
+		
+			<p id="chartname">전기 차 충전소 분표도</p>
+	
+		<article id=middleback>
+			<div id="chart_area"></div>
+		</article>
+	</div>
+		
+		
+		
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+	<script src="https://cdn.amcharts.com/lib/4/core.js"></script>
+	<script src="https://cdn.amcharts.com/lib/4/charts.js"></script>
+	<script src="https://cdn.amcharts.com/lib/4/themes/animated.js"></script>		
+		
+	<script>
+/////////////////////////////////차트///////////////////////////////////////
+	//Pyhton - Spring 연결
+	$(function () {
+		
+		
+		$.ajax({
+			url: "http://127.0.0.1:5000/evChart",
+			type: 'post',
+			dataType: 'json'
+		}).done( function (data) { //데이터를 받으면이쪽으로넘어온다.
+			viewChart(data)
+		}); //fail은 생략
+	});
+
+	function viewChart(json_data) {
+		
+		am4core.useTheme(am4themes_animated);
+	
+		var chart = am4core.create("chart_area", am4charts.XYChart);
+		
+		chart.data = json_data;
+		
+		//x축 만들기
+		var x_Axis = chart.xAxes.push(new am4charts.CategoryAxis());
+		x_Axis.dataFields.category = "city";
+		
+		x_Axis.renderer.labels.template.fontSize = 20;
+		
+		x_Axis.renderer.minGridDistance = 30;
+		
+		//y축 만들기
+		var y_Axis = chart.yAxes.push(new am4charts.ValueAxis());
+		
+		//Series 만들기
+		var series = chart.series.push(new am4charts.ColumnSeries());
+		series.dataFields.categoryX = "city";
+		series.dataFields.valueY = "count";
+		
+		series.columns.template.tooltipText = "[bold]{valueY}[/]";
+		series.columns.template.fill = am4core.color("#85c4b9");
+		series.columns.template.fillOpacity = .7;
+		series.columns.template.stroke = am4core.color("#ffffff");
+	}
+	
+	</script>	
+</body>
+</html>
